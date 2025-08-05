cb6701e810feabc2c3878a4ee73671a9acfa3031
Afonso Santos <afonso.faisca1@gmail.com>
2025-08-05 09:14:25 +0100

    First commit

---
+++ blog/models.py
@@ -0,0 +1,98 @@
+from django import forms
+from django.db import models
+from modelcluster.contrib.taggit import ClusterTaggableManager
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from taggit.models import TaggedItemBase
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.models import Page, Orderable
+from wagtail.snippets.models import register_snippet
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + ["intro"]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request)
+        blog_pages = BlogPage.objects.live().order_by("-first_published_at")
+        context['blog_pages'] = blog_pages
+        return context
+
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    authors = ParentalManyToManyField("blog.Author", blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+
+    def main_image(self):
+        galley_item = self.gallery_images.first()
+
+        if galley_item:
+            return galley_item.image
+
+        return None
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            "date",
+            FieldPanel("authors", widget=forms.CheckboxSelectMultiple),
+            "tags"
+        ], heading="Blog information"),
+        "intro", "body", "gallery_images",
+    ]
+
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name="gallery_images")
+    image = models.ForeignKey(
+        "wagtailimages.Image", on_delete=models.CASCADE, related_name="+"
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = ["image", "caption"]
+
+
+@register_snippet
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    author_image = models.ForeignKey(
+        "wagtailimages.Image", null=True, blank=True,
+        on_delete=models.SET_NULL, related_name="+"
+    )
+
+    panels = ["name", "author_image"]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+
+
+class BlogTagIndexPage(Page):
+
+    def get_context(self, request, *args, **kwargs):
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blog_pages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['blog_pages'] = blog_pages
+
+        return context
+
---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-yb_+qqx%(k_fk7w^!-inot&glx($8!)!ixecpsb=d8f9(hm&eh"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
